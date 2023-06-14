0292fb19cd333b3b98b9cc63eda0f3a514f82781
waffe_x20 <argueta_eduardo@yahoo.de>
2023-06-13 21:11:27 -0600

    Add initial models for Pizza app

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Pizzeria(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.CharField(max_length=512)
+    phone = models.CharField(max_length=40)
+
+
+class Pizza(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=240)
+    thumbnail_url = models.URLField()
+    approved = models.BooleanField(default=False)
+    creator = models.ForeignKey(Pizzeria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
